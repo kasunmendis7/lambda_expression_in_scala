01d9724e37c954f96e90e47fb13f3276063801ae
kasunmendis7 <tkomendis@gmail.com>
2024-08-19 11:15:58 +0530

    matching integers using lambda function

--- integerPatternMatching/IntegerMatch.scala
+++ integerPatternMatching/IntegerMatch.scala
@@ -1,5 +1,13 @@
 object IntegerMatch{
+    def integerMatch(number:Int):String=number match{
+        case n if (n<=0)=>"Negative/Zero"
+        case n if (n%2==0)=>"Even"
+        case _ =>"Odd"
+    }
+
     def main(args: Array[String]):Unit={
-        
+        print("Enter an Integer: ")
+        val input = scala.io.StdIn.readInt()
+        println(f"The integer you entered is ${integerMatch(input)}")
     }
 }
